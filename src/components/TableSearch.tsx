9870d336e92db0cf95adc42727d10afab390c68d
nonlaphan <nonlaphan.pim@devdeva.tech>
2024-11-17 21:18:58 +0700

    set up UI of project

---
+++ src/components/TableSearch.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image"
+
+const TableSearch = () => {
+  return (
+    <div className="w-full md:w-auto flex items-center gap-2 text-xs rounded-full ring-[1.5px] ring-gray-300 px-2">
+    <Image src="/search.png" alt="" height={14} width={14}></Image>
+    <input type="text" placeholder="Search..." className="w-[200px] p-2 bg-transparent outline-none"></input>
+  </div>
+  )
+}
+
+export default TableSearch
